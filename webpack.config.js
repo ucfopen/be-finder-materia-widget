fc95e9e320664eaf8337835fb972e6d6c87a31b0
Ian Turgeon <iturgeon@gmail.com>
2018-10-29 17:35:17 -0400

    switch to default creator

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,12 @@
 // load the reusable legacy webpack config from materia-widget-dev
-module.exports = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig()
+const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
+const entries = widgetWebpack.getDefaultEntries()
+
+delete entries['creator.js']
+delete entries['creator.css']
+
+let options = {
+	entries: entries,
+}
+
+module.exports = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig(options)
